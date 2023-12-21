f882692ffd7071aa82efdf61de3a0d3de062d808
Moodhunter34 <nikospap8@hotmail.com>
2023-12-21 20:49:27 +0000

    hello world version working

--- src/main/java/com/generic/retailer/Cli.java
+++ src/main/java/com/generic/retailer/Cli.java
@@ -3,7 +3,10 @@ package com.generic.retailer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
@@ -56,18 +59,57 @@ public final class Cli implements AutoCloseable {
     writeLine("What would you like to buy?");
     prompt();
     Optional<String> line = readLine();
+    Map<String, Integer> products = new HashMap<>();
+    products.put("books", 0);
+    products.put("cd", 0);
+    products.put("dvd", 0);
+
     while (line.isPresent()) {
+      String user_input = line.get();
+      if ("exit".equalsIgnoreCase(line.get())) {
+        break;
+      }
+      int value = products.get(user_input);
+      products.put(user_input, value + 1);
       writeLine("Would you like anything else?");
       prompt();
       line = readLine();
     }
-    writeLine(String.format("Thank you for visiting Generic Retailer, your total is %s", 0));
+    System.out.println(products);
+    double costs = 0;
+    for (Map.Entry<String, Integer> entry : products.entrySet()) {
+      String key = entry.getKey();
+      Integer quantity = entry.getValue();
+      if (key == "books") {
+        int book_cost = quantity * 5;
+        costs = costs + book_cost;
+        System.out.println("cost of books=" + book_cost);
+
+      } else if (key == "dvd") {
+        int number_of_dvs = quantity / 2 + quantity % 2;
+        double dvd_cost = (double) number_of_dvs * 15;
+        if (quantity % 2 == 1 && date.getDayOfWeek() == DayOfWeek.THURSDAY) {
+          dvd_cost = dvd_cost - 0.2 * 15;
+
+        }
+        costs = costs + dvd_cost;
+
+        System.out.println("cost of dvd=" + dvd_cost);
+      } else if (key == "cd") {
+        int cd_cost = quantity * 10;
+
+        costs = costs + cd_cost;
+        System.out.println("cost of cd=" + cd_cost);
+      }
+    }
+
+    writeLine(String.format("Thank you for visiting Generic Retailer, your total // is %s", costs));
   }
 
   @Override
   public void close() throws Exception {
-      reader.close();
-      writer.close();
+    reader.close();
+    writer.close();
   }
 
 }
