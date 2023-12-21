8d91ef89494df59c2b9eb0a72cfa5eefb4063801
Moodhunter34 <nikospap8@hotmail.com>
2023-12-21 18:57:41 +0000

    original changes

--- src/main/java/com/generic/retailer/Cli.java
+++ src/main/java/com/generic/retailer/Cli.java
@@ -12,105 +12,62 @@ import static java.util.Objects.requireNonNull;
 
 public final class Cli implements AutoCloseable {
 
-    Receipt receipt = new Receipt();
+  public static Cli create(String prompt, BufferedReader reader, BufferedWriter writer, LocalDate date) {
+    requireNonNull(prompt);
+    requireNonNull(reader);
+    requireNonNull(writer);
+    return new Cli(prompt, reader, writer, date);
+  }
 
-    public static Cli create(String prompt, BufferedReader reader, BufferedWriter writer, LocalDate date) {
-        requireNonNull(prompt);
-        requireNonNull(reader);
-        requireNonNull(writer);
-        return new Cli(prompt, reader, writer, date);
+  public static Cli create(BufferedReader reader, BufferedWriter writer) {
+    return new Cli(">", reader, writer, LocalDate.now());
+  }
+
+  private static final Predicate<String> WHITESPACE = Pattern.compile("^\\s{0,}$").asPredicate();
+
+  private final String prompt;
+  private final BufferedReader reader;
+  private final BufferedWriter writer;
+  private final LocalDate date;
+
+  private Cli(String prompt, BufferedReader reader, BufferedWriter writer, LocalDate date) {
+    this.prompt = prompt;
+    this.reader = reader;
+    this.writer = writer;
+    this.date = date;
+  }
+
+  private void prompt() throws IOException {
+    writeLine(prompt);
+  }
+
+  private Optional<String> readLine() throws IOException {
+    String line = reader.readLine();
+    return line == null || WHITESPACE.test(line) ? Optional.empty() : Optional.of(line);
+  }
+
+  private void writeLine(String line) throws IOException {
+    writer.write(line);
+    writer.newLine();
+    writer.flush();
+  }
+
+  public void run() throws IOException {
+    writeLine("What would you like to buy?");
+    prompt();
+    Optional<String> line = readLine();
+    while (line.isPresent()) {
+      writeLine("Would you like anything else?");
+      prompt();
+      line = readLine();
     }
+    writeLine(String.format("Thank you for visiting Generic Retailer, your total is %s", 0));
+  }
 
-    public static Cli create(BufferedReader reader, BufferedWriter writer) {
-        return new Cli(">", reader, writer, LocalDate.now());
-    }
-
-    private static final Predicate<String> WHITESPACE = Pattern.compile("^\\s{0,}$").asPredicate();
-
-    private final String prompt;
-    private final BufferedReader reader;
-    private final BufferedWriter writer;
-    private final LocalDate date;
-
-    private Cli(String prompt, BufferedReader reader, BufferedWriter writer, LocalDate date) {
-        this.prompt = prompt;
-        this.reader = reader;
-        this.writer = writer;
-        this.date = date;
-    }
-
-    private void prompt() throws IOException {
-        writeLine(prompt);
-    }
-
-    private Optional<String> readLine() throws IOException {
-        String line = reader.readLine();
-        return line == null || WHITESPACE.test(line) ? Optional.empty() : Optional.of(line);
-    }
-
-    private void writeLine(String line) throws IOException {
-        writer.write(line);
-        writer.newLine();
-        writer.flush();
-    }
-
-    public void run() throws IOException {
-        writeLine("What would you like to buy? Please enter a product (book, cd, dvd) or type 'exit' to finish.\"");
-        prompt();
-
-        Trolley shoppingCart = new Trolley();
-        Optional<String> productLine = readLine();
-
-        while (productLine.isPresent()) {
-            if ("exit".equalsIgnoreCase(productLine.get())) {
-                break;
-            }
-
-            String product = productLine.get().trim().toLowerCase();
-
-            if (!isValidProduct(product)) {
-                writeLine("Invalid product. Please enter a valid product (e.g., book, cd, dvd) or type 'exit' to finish.");
-            } else {
-                writeLine("How many would you like?");
-                prompt();
-                Optional<String> quantityLine = readLine();
-
-                if (quantityLine.isPresent()) {
-                    try {
-                        int quantity = Integer.parseInt(quantityLine.get().trim());
-                        shoppingCart.ordersProducts(product, quantity);
-                        receipt.addItem(product, quantity);
-                    } catch (NumberFormatException e) {
-                        writeLine("Invalid quantity. Please enter a valid number.");
-                    }
-                } else {
-                    break;  // User entered nothing for quantity, exit the loop
-                }
-            }
-
-            writeLine("Would you like anything else?");
-            prompt();
-            productLine = readLine();
-        }
-
-        // Calculate and display the total
-        double total = shoppingCart.calculateTotal();
-        receipt.setTotal(total);
-        writeLine(String.format("Thank you for visiting Generic Retailer, your total is £%.2f", total));
-
-        writeLine(receipt.printReceipt());
-    }
-
-    private boolean isValidProduct(String product) {
-        // Add more products as needed
-        return "book".equalsIgnoreCase(product) || "cd".equalsIgnoreCase(product) || "dvd".equalsIgnoreCase(product);
-    }
-
-
-    @Override
-    public void close() throws Exception {
-        reader.close();
-        writer.close();
-    }
+  @Override
+  public void close() throws Exception {
+      reader.close();
+      writer.close();
+  }
 
 }
